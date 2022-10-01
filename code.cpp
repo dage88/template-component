891e4631da7c30ad4e663d0dc3ed57b5ba528d9b
Da Ge <dage@gmx.net>
2022-10-01 07:11:44 +0200

    Added some actual cpp class

--- code.cpp
+++ code.cpp
@@ -1,1 +1,12 @@
 #include "code.hpp"
+
+int main(){
+    StateMachine sm;
+    event_t event = OPEN_REQUEST;
+    sm.ProcessEvent(event);
+    sm.ProcessEvent(event);
+    event = CLOSE_REQUEST;
+    sm.ProcessEvent(event);
+    sm.ProcessEvent(event);
+    return 0;
+}
--- test/test.cpp
+++ test/test.cpp
@@ -5,5 +5,5 @@ TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+  EXPECT_EQ(1+1, 5) << "some really fancy calculation here";
 }
