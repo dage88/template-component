a1f4b4f62d00a5e754ab300f332229471db548af
Da Ge <dage@gmx.net>
2022-11-06 19:07:11 +0100

    Add content to main loop and add executable

--- template_main.cpp
+++ template_main.cpp
@@ -4,10 +4,18 @@
 int main(){
     StateMachine    sm;
     event_t         event = EVENT_INITIALIZE_REQUEST;
+    int counter = 0;
 
     sm.ProcessEvent(event);
 
-    while(true);
+    while(true)
+    {
+        counter++;
+        if (counter==0)
+        {
+            std::cout << "Overflow.";
+        }
+    };
 
     std::cout << "main end.";
     return 0;
